f5da1228e9c6aa5db59bf4c0dafa68e546da74c8
otarampinelli <otaviorampinellielias@gmail.com>
2020-12-05 15:55:12 -0300

    unique key option

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -28,7 +28,7 @@ const SearchParams = () => {
           >
             <option>All</option>
             {ANIMALS.map((animal) => (
-              <option value={animal}>{animal}</option>
+              <option key={animal} value={animal}>{animal}</option>
             ))}
           </select>
         </label>
